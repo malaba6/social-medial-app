43e4c40f13a52175aefb6fe05c9ef42f77fddb16
Malaba Eric <50101879+Malaba6@users.noreply.github.com>
2023-02-21 10:45:28 +0300

    refactor code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'Social media sample App'
+
+    if base_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
